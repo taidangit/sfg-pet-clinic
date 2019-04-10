c1a6b64f93ff0291f6e4df52fc661cb1d32ddb50
ADMIN <dangphattai92@gmail.com>
2019-04-10 21:59:24 +0700

    impl map base service. Closes #6.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/CrudService.java
@@ -8,6 +8,8 @@ public interface CrudService<T, ID> {
 
     T findById(ID id);
 
+    T save(T object);
+
     void delete(T object);
 
     void deleteById(ID id);
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/PetMapService.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class PetMapService extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> finadAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
